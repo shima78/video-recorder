2a53918394d7b7c6b07ef5834ed7c5856278fad3
Zahra Mohammadpour <zahramohammadpour497@gmail.com>
2023-08-22 16:55:11 +0330

    feat: video recorder app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import VideoRecorder from "./components/VideoRecorder.vue";
+</script>
+
+<template>
+  <img class="logo" src="./assets/logo.svg" alt="Logo" />
+  <VideoRecorder :timeout="10000" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em rgba(105, 100, 255, 0.67));
+}
+
+</style>
---
+++ src/components/VideoRecorder.vue
@@ -0,0 +1,94 @@
+<script setup lang="ts">
+import { ref, onBeforeUnmount, Ref } from "vue";
+const video = ref();
+const videoStream = ref();
+const mediaRecorder = ref();
+const recordedChunks: Ref<Array<any>> = ref([]);
+const videoBlob = ref();
+const recording = ref(false);
+const uploading = ref(false);
+const uploadProgress = ref(0);
+
+interface Props {
+  timeout: number;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  timeout: 30000,
+});
+
+async function startRecording() {
+  try {
+    videoStream.value = await navigator.mediaDevices.getUserMedia({
+      video: true,
+    });
+    recordedChunks.value = [];
+    video.value.srcObject = videoStream.value;
+    mediaRecorder.value = new MediaRecorder(videoStream.value);
+
+    mediaRecorder.value.ondataavailable = (event: any) => {
+      if (event.data.size > 0) {
+        recordedChunks.value.push(event.data);
+      }
+    };
+
+    mediaRecorder.value.onstop = () => {
+      videoBlob.value = new Blob(recordedChunks.value, { type: "video/webm" });
+      URL.createObjectURL(videoBlob.value);
+    };
+
+    mediaRecorder.value.start();
+    recording.value = true;
+
+    setTimeout(() => {
+      stopRecording();
+    }, props.timeout); // Stop recording after timeout
+  } catch (error) {
+    console.error("Error starting recording:", error);
+  }
+}
+
+function stopRecording() {
+  if (mediaRecorder.value && recording.value) {
+    mediaRecorder.value.stop();
+    videoStream.value.getTracks().forEach((track: any) => track.stop());
+    recording.value = false;
+  }
+}
+
+async function uploadVideo() {
+  if (videoBlob.value) {
+    // Simulate uploading process
+    uploading.value = true;
+    for (let progress = 0; progress <= 100; progress += 10) {
+      uploadProgress.value = progress;
+      await new Promise((resolve) => setTimeout(resolve, 5000));
+    }
+    uploading.value = false;
+    uploadProgress.value = 0;
+
+    // Here, we implement actual uploading logic to the server
+  }
+}
+
+// Clean up when the component is unmounted
+onBeforeUnmount(() => {
+  if (videoStream.value) {
+    videoStream.value.getTracks().forEach((track: any) => track.stop());
+  }
+});
+</script>
+
+<template>
+  <h2>Video Recorder</h2>
+  <video ref="video" autoplay></video>
+
+  <div class="card">
+    <button @click="startRecording" :disabled="recording">Record</button>
+    <button @click="stopRecording" :disabled="!recording">Stop</button>
+    <button @click="uploadVideo" :disabled="!videoBlob">
+      Upload Recorded File
+    </button>
+  </div>
+  <progress v-if="uploading" :value="uploadProgress" max="100"></progress>
+</template>
